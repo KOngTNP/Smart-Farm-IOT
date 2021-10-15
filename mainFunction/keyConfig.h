8d4f69006a5000ea53173c52f4354c9eaf852d83
Tanapol Buangam <49057759+KOngTNP@users.noreply.github.com>
2021-10-15 08:16:08 +0700

    Update keyConfig.h

--- mainFunction/keyConfig.h
+++ mainFunction/keyConfig.h
@@ -9,17 +9,17 @@
 
 //**************************************************************************************************//
 
-const char* ssid = "KOng-wifi"; // <<<  input wifi name
-const char* pass = "55554321";  // <<< input wifi password
+const char* ssid = ""; // <<<  input wifi name
+const char* pass = "";  // <<< input wifi password
 
 #ifdef  BLYNKLOCAL
 #define serverBlynk "blynk.iot-cm.com" // <<< input blynk server name
 #define portBlynk 8080
-const char* auth = "YCc1UGEgkHjFRLIYQ65mLLG3CwXS8a8X";  // <<< input blynk local key
+const char* auth = "";  // <<< input blynk local key
 
 // if you want to use official blynk
 #else
-const char* auth = "gk285LJOLrfff5984by5MSY25gt9eYBH";  // <<< input blynk official
+const char* auth = "";  // <<< input blynk official
 #endif
 
 //lora setting
@@ -28,20 +28,20 @@ byte destination = 0xA9;      // lora destination to send to
 
 //line notify setting
 #ifdef LINENOTIFY                                               
-#define LINE_TOKEN  "M7XPWWnMZi5JbWB5zBFYcMZIYj6udtCPLQqBlHmbQrr"  // <<< input line token
+#define LINE_TOKEN  ""  // <<< input line token
 #endif
 
 // weather setting form openweathermap
 #ifdef WEATHER
 const String weatherHost = "http://api.openweathermap.org/data/2.5/weather?id=";
-const char* weatherKey = "b6ca45ff002e68ab7ab98b3c31f18ec6";  // <<< input weather key
-//const char* cityID = "1611110";  // <<< input city id, In this case is id of Chon Buri, Thailand
+const char* weatherKey = "";  // <<< input weather key
+//const char* cityID = "";  // <<< input city id, In this case is id of Chon Buri, Thailand
 #endif
 
 // friebase setting
 #ifdef FIREBASE
-const char* firebaseHost = "smart-farm-5d39d-default-rtdb.asia-southeast1.firebasedatabase.app"; // input friebase server
-const char* firebaseAuth = "BxLmeucqHzAFKmhO7H6Ne4e9CD9SDWUnC2bztefF"; // <<< input friebase key
+const char* firebaseHost = ""; // input friebase server
+const char* firebaseAuth = ""; // <<< input friebase key
 #endif
 
 //  end code //
